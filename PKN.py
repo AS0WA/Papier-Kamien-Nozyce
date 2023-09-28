759c42f28ec4737b42bd768551a272b0ef4272b0
AS0WA <antonisiwiorek@gmail.com>
2023-09-28 10:47:21 +0200

    Create PKN.py

---
+++ PKN.py
@@ -0,0 +1,57 @@
+import random
+
+player_points = 0
+computer_points = 0
+
+def paper():
+    global player_points, computer_points
+    if player_symbol == 'p' and computer_symbol == 'k':
+        print('wygrywasz')
+        player_points += 1
+    elif player_symbol == 'p' and computer_symbol == 'n':
+        print('przegrywasz')
+        computer_points += 1
+    else: print('remis')
+
+def rock():
+    global player_points, computer_points
+    if player_symbol == 'k' and computer_symbol == 'n':
+        print('wygrywasz')
+        player_points += 1
+    elif player_symbol == 'k' and computer_symbol == 'p':
+        print('przegrywasz')
+        computer_points += 1
+    else: print('remis')
+
+def scissors():
+    global player_points, computer_points
+    if player_symbol == 'n' and computer_symbol == 'p':
+        print('wygrywasz')
+        player_points += 1
+    elif player_symbol == 'n' and computer_symbol == 'k':
+        print('przegrywasz')
+        computer_points += 1
+    else: print('remis')
+
+symbols = ['p', 'k', 'n']
+
+while True:
+    while True:
+        player_symbol = input('Podaj znak: papier(p), kamień(k), nożyce(n):''\n' )
+        if player_symbol != 'p' and player_symbol != 'k' and player_symbol != 'n':
+            print('Nie poprawna litera')
+        else: break
+
+    computer_symbol = random.choice(symbols)
+
+    print(computer_symbol)
+
+    if player_symbol == 'p':
+        paper()
+    elif player_symbol == 'k':
+        rock()
+    else:
+        scissors()
+
+    print('Twoje punkty: ' + str(player_points))
+    print('Punkty komputera ' + str(computer_points))
