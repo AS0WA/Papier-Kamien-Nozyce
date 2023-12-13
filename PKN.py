83d42b423a2667b24f5a36710274db8c26506761
Antoni Siwiorek <146232484+AS0WA@users.noreply.github.com>
2023-12-13 17:21:30 +0100

    Update PKN.py

--- PKN.py
+++ PKN.py
@@ -1,46 +1,16 @@
 import random
 
+
+symbols = ['p', 'k', 'n']
+round_count = 0
 player_points = 0
 computer_points = 0
 
-def paper():
-    global player_points, computer_points
-    if player_symbol == 'p' and computer_symbol == 'k':
-        print('wygrywasz')
-        player_points += 1
-    elif player_symbol == 'p' and computer_symbol == 'n':
-        print('przegrywasz')
-        computer_points += 1
-    else:
-        print('remis')
-
-def rock():
-    global player_points, computer_points
-    if player_symbol == 'k' and computer_symbol == 'n':
-        print('wygrywasz')
-        player_points += 1
-    elif player_symbol == 'k' and computer_symbol == 'p':
-        print('przegrywasz')
-        computer_points += 1
-    else:
-        print('remis')
-
-def scissors():
-    global player_points, computer_points
-    if player_symbol == 'n' and computer_symbol == 'p':
-        print('wygrywasz')
-        player_points += 1
-    elif player_symbol == 'n' and computer_symbol == 'k':
-        print('przegrywasz')
-        computer_points += 1
-    else:
-        print('remis')
-
-symbols = ['p', 'k', 'n']
-
 while True:
+    round_count += 1
     while True:
-        player_symbol = input('Podaj znak: papier(p), kamień(k), nożyce(n):''\n' )
+        print(f"Runda numer: {round_count}")
+        player_symbol = input('Podaj znak: papier(p), kamień(k), nożyce(n): \n')
         if player_symbol != 'p' and player_symbol != 'k' and player_symbol != 'n':
             print('Nie poprawna litera')
         else:
@@ -51,11 +21,32 @@ while True:
     print(computer_symbol)
 
     if player_symbol == 'p':
-        paper()
+        if computer_symbol == 'k':
+            print('wygrywasz')
+            player_points += 1
+        elif computer_symbol == 'n':
+            print('przegrywasz')
+            computer_points += 1
+        else:
+            print('remis')
     elif player_symbol == 'k':
-        rock()
+        if computer_symbol == 'n':
+            print('wygrywasz')
+            player_points += 1
+        elif computer_symbol == 'p':
+            print('przegrywasz')
+            computer_points += 1
+        else:
+            print('remis')
     else:
-        scissors()
+        if computer_symbol == 'p':
+            print('wygrywasz')
+            player_points += 1
+        elif computer_symbol == 'k':
+            print('przegrywasz')
+            computer_points += 1
+        else:
+            print('remis')
 
-    print('Twoje punkty: ' + str(player_points))
-    print('Punkty komputera ' + str(computer_points))
+    print(f'Twoje punkty: {str(player_points)}')
+    print(f'Punkty komputera: {str(computer_points)}\n')
